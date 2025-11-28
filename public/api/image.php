26c657d36ea0c71ed2b27431952f99646f9d23ce
Blackpig <stuart@blackpig.eu>
2025-11-28 17:46:49 +0100

    Fix vendor autoload path

--- public/api/image.php
+++ public/api/image.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
 $dotenv->load();
 
 header('Content-Type: application/json');
